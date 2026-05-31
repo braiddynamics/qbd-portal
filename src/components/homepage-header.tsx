fa3ff311c81ba7f73f763e361de777ead3d6e2ec
BraidDynamics <braiddynamics@gmail.com>
2026-05-31 06:24:57 -0400

    fix: header

--- src/components/homepage-header.tsx
+++ src/components/homepage-header.tsx
@@ -178,7 +178,7 @@ export default function HomepageHeader() {
               className="button button--secondary button--lg btn-pulse-white-active" 
               to="/monograph"
             >
-              The Monograph
+              Table of Contents
             </Link>
             <Link 
               className="button hero-white-btn button--lg btn-pulse-white-active" 
